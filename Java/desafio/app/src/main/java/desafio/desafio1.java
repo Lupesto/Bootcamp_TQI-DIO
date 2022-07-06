90b6ffb87c5eff3a76f9bca4ba7ec16bf47858fb
lupesto <lucasthe2@gmail.com>
2022-07-06 10:35:48 -0300

    react

---
+++ Java/desafio/app/src/main/java/desafio/desafio1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package desafio;
+
+/**
+ *
+ * @author Lucas
+ */
+public class desafio1 {
+    public static void main(String[] args) {
+        
+        int[] idades= {1,2,99};
+        for (int i = 0; i < idades.length; i++) {
+            System.out.println(idades[i]);
+        }
+        
+        
+    }
+}
