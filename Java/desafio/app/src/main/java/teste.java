f9a2c1d29951bd5623b62dad040aaa50b5a01955
lupesto <lucasthe2@gmail.com>
2022-07-08 20:29:14 -0300

    general

---
+++ Java/desafio/app/src/main/java/teste.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author Lucas
+ */
+public class teste {
+    
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String x1y1 = scan.nextLine();
+        //System.out.println(x1y1);
+        //String ascan = scan.nextLine();
+        String x2y2 = scan.nextLine();
+        //System.out.println(x2y2);
+
+        String[] aux = x1y1.split(" ");
+        String[] aux1 = x2y2.split(" ");
+
+        float x1 = Float.parseFloat(aux[0]);
+        float y1 = Float.parseFloat(aux[1]);
+        float x2 = Float.parseFloat(aux1[0]);
+        float y2 = Float.parseFloat(aux1[1]);
+
+        //System.out.println("(" + x2 + " - " + x1 + ")² + ( "+ y2 + " - " + y1 + ")²");
+        System.out.printf("%.4f",  Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+
+    }
+}
+
